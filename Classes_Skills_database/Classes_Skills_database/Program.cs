f48540c940b0d92f825189f120b7942594cc05f9
Isantanna <igor_sant@hotmail.com>
2021-06-24 12:08:58 -0300

    Commit Inicial

---
+++ Classes_Skills_database/Classes_Skills_database/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes_Skills_database.Vocations;
+
+namespace Classes_Skills_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adventures akadians = new Adventures();
+
+            akadians.Nome = "Akadians Newseed";
+            akadians.Level = 53;
+            akadians.Classe = "Druid";
+            akadians.Raça = "Tauren";
+            akadians.Arma = "Cajado";
+            akadians.Titulo = "LifeProtector";
+
+
+
+        }
+    }
+}
---
+++ Classes_Skills_database/Classes_Skills_database/Vocations/Adventures.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Skills_database.Vocations
+{
+    class Adventures
+    {
+        public String Nome { get; set; }
+        public int Level { get; set; }
+        public String Classe { get; set; }
+        public String Raça { get; set; }
+        public String Arma { get; set; }
+        public String Titulo { get; set; }
+
+        
+
+
+
+        
+    }
+}
